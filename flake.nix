de3b0f2d489fcc7d09f349c9277dc50181d0a74e
MaxCan-Code <35848843+MaxCan-Code@users.noreply.github.com>
2025-02-05 20:45:35 -0500

    Reproducible flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 {
-  outputs = inputs: {
-    devShells.x86_64-linux.default = import ./shell.nix;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    devShells.x86_64-linux.default = import ./shell.nix {inherit nixpkgs;};
   };
 }
--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-  pkgs = import nixpkgs {
+{
+  nixpkgs ? fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11",
+  pkgs ? import nixpkgs {
+    system = "x86_64-linux";
     config =  {
       permittedInsecurePackages = ["freeimage-unstable-2021-11-01"]; # This is a dependency of nixpkgs.arrayfire
       allowUnfree = true; # For nixpkgs.dyalog
@@ -11,8 +12,8 @@ let
       };
     };
     overlays = [];
-  };
-in
+  },
+}:
 pkgs.mkShell.override {
   stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
 } {
